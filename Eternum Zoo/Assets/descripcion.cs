a9333b6d26d200e13826ace63a13d3114776eb6b
FranVusm <130254464+FranVusm@users.noreply.github.com>
2023-10-22 13:22:42 -0300

    Arma toda hack

---
+++ Eternum Zoo/Assets/descripcion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class descripcion : MonoBehaviour
+{
+    public Image image;
+    public GameObject imagen;
+    public GameObject imagen2;
+    public GameObject imagen3;
+    public GameObject imagen4;
+
+    void Update()
+    {
+    }
+}
+
