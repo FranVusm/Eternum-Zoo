1e04d32dd28417365c14e7981677d05be5609a9a
FranVusm <130254464+FranVusm@users.noreply.github.com>
2023-12-01 20:32:00 -0300

    segunda arma. más cambio de la misma

---
+++ Eternum Zoo/Assets/main_juego/Prefrab/prefab_esco/escopeta.cs
@@ -0,0 +1,40 @@
+ 
+using UnityEngine;
+
+public class escopeta : MonoBehaviour
+{
+    public GameObject[] spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject[] accesorios = WeaponControl.Instance.accesorios_esco;
+        int[] spawn_content = WeaponControl.Instance.spauner_esco;
+
+        for (int i = 0; i < spawn.Length; i++)
+
+        {
+            if (spawn_content[i] == -1)
+            {
+                continue;
+            }
+            print(spawn_content[i]);
+            Vector3 spawnPosition = spawn[i].transform.position;
+            Quaternion spawnRotation = spawn[i].transform.rotation;
+
+            // Instancia el objeto en la posición y rotación del spawn[i]
+            GameObject newAccesorio = Instantiate(accesorios[spawn_content[i]], spawnPosition, spawnRotation);
+
+            // Mantén la escala del accesorio
+            Vector3 accesorioScale = accesorios[spawn_content[i]].transform.localScale;
+            newAccesorio.transform.localScale = accesorioScale;
+
+            // Establece el padre del objeto recién instanciado para mantenerlo unido
+            newAccesorio.transform.parent = spawn[i].transform;
+
+
+        }
+
+    }
+}
--- Eternum Zoo/Assets/main_juego/scrips/InvenContru/WeaponControl.cs
+++ Eternum Zoo/Assets/main_juego/scrips/InvenContru/WeaponControl.cs
@@ -5,8 +5,9 @@ public class WeaponControl : MonoBehaviour
     public static WeaponControl Instance;
 
     public GameObject[] accesorios;
+    public GameObject[] accesorios_esco;
     public int[]  spauner = new int[4];
-   
+    public int[] spauner_esco = new int[5];
     void Awake()
     {
         if (Instance == null)
