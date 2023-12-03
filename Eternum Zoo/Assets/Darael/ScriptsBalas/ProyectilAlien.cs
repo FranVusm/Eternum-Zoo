408c246dcdaaf51dcec42fe7e3cf7114c72d13fc
FranVusm <130254464+FranVusm@users.noreply.github.com>
2023-12-03 15:54:23 -0300

    feature not bug :D

--- Eternum Zoo/Assets/Darael/ScriptsBalas/ProyectilAlien.cs
+++ Eternum Zoo/Assets/Darael/ScriptsBalas/ProyectilAlien.cs
@@ -5,43 +5,35 @@ using UnityEngine;
 public class ProyectilAlien : MonoBehaviour
 {
     public int danio = 10; // Cantidad de daño que inflige el proyectil del Alien
-
+     
 
     private void OnTriggerEnter(Collider other)
     {
         
         // Verifica si el objeto con el que colisionó tiene el tag "enemigo"
-        if (other.CompareTag("enemigo"))
-        {
-            
-            Destroy(gameObject);
-        }
-
-        if (other.CompareTag("NPC"))
-        {
-             
-            Destroy(gameObject);
-        }
+        
         if (other.CompareTag("Player"))
         {
-
+            print("hit");
             SistemaVida sistemaVida = other.GetComponent<SistemaVida>();
             if (sistemaVida != null)
             {
+                
                 sistemaVida.RecibirDanio(danio);
-                Destroy(gameObject);
+                
             }
             // Destruye el proyectil al impactar con el enemigo
-
-
-
-        }
-
-        if (other.CompareTag("Candado"))
-        {
-             
             Destroy(gameObject);
+
+
         }
+
+        if (!other.CompareTag("Player"))
+        {
+            Destroy(gameObject);    
+        }
+
+           
 
     }
 }
