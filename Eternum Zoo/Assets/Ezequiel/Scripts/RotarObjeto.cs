03f5ead28f95317b7e42dac4099d490fd2223797
Darael <dbadilla@alumnos.inf.utfsm.cl>
2023-12-03 21:45:06 -0300

    Se actualizaron las canciones

--- Eternum Zoo/Assets/Ezequiel/Scripts/RotarObjeto.cs
+++ Eternum Zoo/Assets/Ezequiel/Scripts/RotarObjeto.cs
@@ -7,10 +7,39 @@ public class RotarObjeto : MonoBehaviour
 
     private bool rotacionActiva = false;
     private int n = 0;
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+
+    // Variables para controlar si la música está reproduciéndose
+    private bool isPlayingAudio1 = false;
+    private bool isPlayingAudio2 = false;
+    private bool cambioRealizado = false;
 
     public void ActivarRotacion()
     {
         rotacionActiva = true;
+        // Si ya se realizó un cambio, salir del método
+        if (cambioRealizado)
+        {
+            //Debug.Log("El cambio ya se realizó. No se puede cambiar nuevamente.");
+            return;
+        }
+
+        if (isPlayingAudio1)
+        {
+            // Si audioSource1 está reproduciéndose, detenerlo y reproducir audioSource2
+            audioSource1.Stop();
+            audioSource2.Play();
+            isPlayingAudio1 = false;
+            isPlayingAudio2 = true;
+        }
+        // Marcar que el cambio se ha realizado
+        cambioRealizado = true;
+    }
+    void Start(){
+        audioSource1.Play();
+        audioSource2.Stop();
+        isPlayingAudio1 = true;
     }
 
     void Update()
